d920f74a4acb1a06c85c3554eb0e722e97d3b98a
Dageshwar Das <dageshwardasmanikpuri996@gmail.com>
2023-03-10 09:42:07 +0530

    ho gya

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const app = express();
 const cors = require("cors");
 require("./db/conn");
 const router = require("./Routes/router");
-const port = 6002;
+
+const port = process.env.PORT || 6002;
 
 
 app.use(cors());
@@ -13,5 +14,5 @@ app.use(express.json());
 app.use(router)
 
 app.listen(port,()=>{
-    console.log("server start");
+    console.log(`server start on port ${port}`);
 })
